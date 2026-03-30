b7fd10692ccce5f3226351e9c0437e6f101a3eec
Andy Fragen <andy@thefragens.com>
2026-03-30 09:27:59 -0700

    heroImage now featuredImage

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,7 @@ const blog = defineCollection({
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
+			featuredImage: z.optional(image()),
 			categories: z.array(z.string()).optional(),
 			draft: z.boolean().optional(),
 		}),
